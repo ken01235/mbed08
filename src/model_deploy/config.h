dc6dc2ad3fe5edad9a08940221fb849c1db2debc
ken01235 <kenkenrc08@gmail.com>
2021-04-20 22:01:33 -0700

    .

--- src/model_deploy/config.h
+++ src/model_deploy/config.h
@@ -5,7 +5,7 @@
 
 // The number of labels (without negative)
 
-#define label_num 2
+#define label_num 3
 
 
 struct Config {
@@ -18,44 +18,37 @@ struct Config {
 
   // The number of expected consecutive inferences for each gesture type.
 
-  const int consecutiveInferenceThresholds[label_num] = {20, 10};
+  const int consecutiveInferenceThresholds[label_num] = {20, 10, 10};
 
 
   const char* output_message[label_num] = {
 
         "RING:\n\r"
-
         "          *       \n\r"
-
         "       *     *    \n\r"
-
         "     *         *  \n\r"
-
         "    *           * \n\r"
-
         "     *         *  \n\r"
-
         "       *     *    \n\r"
-
         "          *       \n\r",
-
         "SLOPE:\n\r"
-
         "        *        \n\r"
-
         "       *         \n\r"
-
         "      *          \n\r"
-
         "     *           \n\r"
-
         "    *            \n\r"
-
         "   *             \n\r"
-
         "  *              \n\r"
-
-        " * * * * * * * * \n\r"};
+        " * * * * * * * * \n\r",
+        "LINE:\n\r"
+        "                 \n\r"
+        "                 \n\r"
+        "                 \n\r"
+        " * * * * * * * * \n\r"
+        "                 \n\r"
+        "                 \n\r"
+        "                 \n\r"
+        "                 \n\r"};
 
 };
 
